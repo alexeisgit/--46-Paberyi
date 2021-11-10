562d216bcf8ef89424a7d9426ce39b6bcb4b0ec2
al <alex.pobery@gmail.com>
2021-11-09 20:10:41 -0800

    Тест 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,14 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClass {
+
+    private int class_number = 20;
+
+    public int getClassNumber(){
+
+        return class_number;
+    }
 
     @Test
     public void testGetLocalNumber()
@@ -17,4 +25,17 @@ public class MainClass {
     public int getLocalNumber() {
         return 14;
     }
+
+    @Test
+    public void testGetClassNumber() {
+        int a = getClassNumber();
+        if (a > 45) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed, number is less or equal 45 ");
+            //Assert.fail("Test Failed, number is less or equal 45");
+        }
+    }
 }
+
+
