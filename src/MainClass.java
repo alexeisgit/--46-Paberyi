644e489e51ad4b463aaeaac209714129648fc04b
al <alex.pobery@gmail.com>
2021-11-09 20:55:58 -0800

    Тест 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,10 +3,15 @@ import org.junit.Test;
 
 public class MainClass {
 
+    private String class_string = "Hello, world";
+    public String getClassString()
+    {
+        return class_string;
+    }
+
     private int class_number = 20;
-
-    public int getClassNumber(){
-
+    public int getClassNumber()
+    {
         return class_number;
     }
 
@@ -27,13 +32,30 @@ public class MainClass {
     }
 
     @Test
-    public void testGetClassNumber() {
+    public void testGetClassNumber()
+    {
         int a = getClassNumber();
         if (a > 45) {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed, number is less or equal 45 ");
             //Assert.fail("Test Failed, number is less or equal 45");
+        }
+    }
+
+    @Test
+    public void testGetClassString()
+    {
+        String a = getClassString();
+        if (a.contains("Hello")) {
+            System.out.println("Test passed");
+
+        } else if (a.contains("hello")){
+            System.out.println("Test passed");
+
+        } else {
+            System.out.println("Test Failed: string not found");
+
         }
     }
 }
